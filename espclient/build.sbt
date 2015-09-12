5a7038a4e1f0f2fd3bce7fa298ff50492f2d2fce
jcarley <jcarley@gettyimages.com>
2015-09-12 09:06:32 -0500

    Updating the espclient kata

--- espclient/build.sbt
+++ espclient/build.sbt
@@ -7,11 +7,9 @@ version := "0.0.1"
 scalaVersion := "2.11.5"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "2.2.1" % "test" withSources() withJavadoc(),
-  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test" withSources() withJavadoc(),
   "org.apache.httpcomponents" % "httpclient" % "4.5",
-  "org.json4s" %% "json4s-native" % "3.2.11",
-  "org.json4s" %% "json4s-jackson" % "3.2.11"
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test" withSources() withJavadoc(),
+  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test" withSources() withJavadoc()
 )
 
 initialCommands := "import com.example.espclient._"
--- espclient/src/main/scala/HelloWorld.scala
+++ espclient/src/main/scala/HelloWorld.scala
@@ -1,12 +1,47 @@
 package com.example.espclient
 
-import org.json4s._
-import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
+import java.io._
+import org.apache.http.{HttpEntity, HttpResponse}
+import org.apache.http.client._
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+import scala.collection.mutable.StringBuilder
+import org.apache.http.params.HttpConnectionParams
+import org.apache.http.params.HttpParams
 
 object HelloWorld {
+  // client_id: 'y8u59tcu9gy7jmmv8jrcwbyf'
+  // client_secret: '6FTGN3GpECXQkgRhBgVVwsJwQPAe9JGzYpTbT8aEVaaVr'
+  // esp_username: 'jeff.carley@gettyimages.com'
+  // esp_password: 'letmein123'
+  // base_uri: 'https://esp-sandbox.api.gettyimages.com/esp'
+  //
+  // uri = URI('https://api.gettyimages.com/oauth2/token')
+
   def main(args: Array[String]) {
-    println("Hello World!!!")
+  }
+
+  def getRestContent(url: String, connectionTimeout: Int, socketTimeout: Int): String = {
+      val httpClient = buildHttpClient(connectionTimeout, socketTimeout)
+      val httpResponse = httpClient.execute(new HttpGet(url))
+      val entity = httpResponse.getEntity
+      var content = ""
+      if (entity != null) {
+        val inputStream = entity.getContent
+        content = io.Source.fromInputStream(inputStream).getLines.mkString
+        inputStream.close
+      }
+      httpClient.getConnectionManager.shutdown
+      content
+  }
+
+  def buildHttpClient(connectionTimeout: Int, socketTimeout: Int): DefaultHttpClient = {
+    val httpClient = new DefaultHttpClient
+    val httpParams = httpClient.getParams
+    HttpConnectionParams.setConnectionTimeout(httpParams, connectionTimeout)
+    HttpConnectionParams.setSoTimeout(httpParams, socketTimeout)
+    httpClient.setParams(httpParams)
+    httpClient
   }
 }
 
