654308620020c2b7c96473469ea4ea9aac044271
jcarley <jcarley@gettyimages.com>
2015-09-12 09:05:36 -0500

    Updating the product search kata

--- productsearch/build.sbt
+++ productsearch/build.sbt
@@ -3,12 +3,15 @@ name := "productsearch"
 organization := "com.example"
 
 version := "0.0.1"
-
 scalaVersion := "2.11.5"
+val json4sVersion = "3.2.10"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "2.2.1" % "test" withSources() withJavadoc(),
-  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test" withSources() withJavadoc()
+  "org.json4s" %% "json4s-native" % json4sVersion,
+  "org.json4s" %% "json4s-ext" % json4sVersion,
+  "org.json4s" %% "json4s-jackson" % json4sVersion,
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
 )
 
 initialCommands := "import com.example.productsearch._"
--- productsearch/src/main/scala/HelloWorld.scala
+++ productsearch/src/main/scala/HelloWorld.scala
@@ -1,51 +1,102 @@
 package com.example.productsearch
 
-import scala.collection.mutable.AbstractMap
-import scala.collection.mutable.HashMap
+import java.io._
+import scala.collection.mutable.{Map, HashMap}
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
+case class Product(name: String, price: String, quantity: Int)
+object Product {
+  def fromMap(map: Map[String, String]): Product =
+    new Product(map.get("product").head, map.get("price").head, map.get("quantity").head.toInt)
+}
 
 object HelloWorld {
 
   def main(args: Array[String]) {
 
-    val searcher = createSearcher { () => openDataFile("mydatafile.json") }
+    var searcher = createSearcher { () => openDataFile(source()) }
 
-    val result = prompt(searcher)
+    run {(criteria: String) => {
 
-    println(result)
-  }
+      val (result, isFound) = searcher(criteria)
 
-  def prompt(searcher: (String) => String) = {
-    println("What is the product name? ")
-    searcher("truck")
-  }
-
-  def saveDataFile(path: String, product: String, price: String, quantity: Int) {
-      val data = openDataFile(path)
-  }
-
-  def createSearcher(data: () => AbstractMap[String, String]) = {
-
-    val dataSet = data()
-
-    (criteria: String) = (String) => String = {
-      val result = dataSet.get(criteria)
-      if(result.isEmpty) {
-        return ""
+      if(isFound) {
+        println(result)
+      } else {
+        searcher = createSearcher { () => saveDataFile(source()) }
       }
-      result.get
+
+    }}
+
+  }
+
+  def run(action: (String) => Unit) {
+    while(true) {
+      val input = prompt("What is the product name? (:q to quit) ")
+      if(input == ":q") {
+        return
+      } else {
+        action(input)
+      }
     }
   }
 
-  def openDataFile(path: String) = {
+  def source() = {
+    io.Source.fromFile("products.json")
+  }
 
-    // open data file
-    val fileName = path.toUpperCase()
+  def prompt(msg: String) = {
+    print(msg)
+    val lines = io.Source.stdin.getLines()
+    val input = lines.next()
+    input
+  }
 
-    // parse into a HashMap
-    val dataSet = new HashMap[String, String]()
-    dataSet.update("truck", "price:10.00")
-    dataSet.update("ball", "price:5.00")
-    dataSet.update("crayon", "price:7.00")
+  def createSearcher(data: () => Map[String, Product]) = {
+
+    val dataSet = data()
+
+    (criteria: String) => {
+      val result = dataSet.get(criteria.toLowerCase())
+      (result.getOrElse(()=>"not found"), result.nonEmpty)
+    }
+  }
+
+  def saveDataFile(source: io.Source) = {
+
+      val choices = Array("product", "price", "quantity")
+
+      val dataMap =
+        (Map[String, String]() /: choices) { (map, choice) =>
+          val input = prompt(s"What is the ${choice}: ")
+          map + (choice -> input)
+        }
+      val product = Product.fromMap(dataMap)
+
+      // save to hash
+      val data = openDataFile(source)
+      data.update(product.name, product)
+
+      //TODO: save hash to file
+
+      data
+  }
+
+  def openDataFile(source: io.Source) = {
+
+    implicit val formats = DefaultFormats
+
+    val json = source.mkString
+
+    val products = parse(json)
+
+    val dataSet =
+      (Map[String, Product]() /: (products \ "products").extract[List[Product]]) { (map, item) =>
+        map + (item.name.toLowerCase() -> item)
+      }
 
     dataSet
   }
