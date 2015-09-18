2716001ef35b34dfc37eb3ab342df91dec3ef596
jcarley <jcarley@gettyimages.com>
2015-09-17 21:25:37 -0500

    Initial components for product inventory kata

---
+++ productinventory/build.sbt
@@ -0,0 +1,23 @@
+name := "productinventory"
+
+organization := "com.example"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.5"
+
+val json4sVersion = "3.2.10"
+
+libraryDependencies ++= Seq(
+  "org.json4s" %% "json4s-native" % json4sVersion,
+  "org.json4s" %% "json4s-ext" % json4sVersion,
+  "org.json4s" %% "json4s-jackson" % json4sVersion,
+  "org.apache.httpcomponents" % "httpclient" % "4.5",
+  "org.apache.httpcomponents" % "fluent-hc" % "4.5",
+  "org.apache.commons" % "commons-lang3" % "3.4",
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
+)
+
+initialCommands := "import com.example.productinventory._"
+
---
+++ productinventory/startup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#
+# This script will setup your environment to run docker.  It sets the proper
+# environment variables.
+#
+# Usage: source ./startup.sh
+#
+
+set +e
+
+docker-machine status dev &>/dev/null
+
+if [ $? != 0 ]; then
+  docker-machine create --driver vmwarefusion dev
+fi
+
+if test $(docker-machine status dev) == "Stopped"; then
+  echo "# Not running dev...starting up now."
+  docker-machine start dev >/dev/null
+fi
+
+echo "# Running the \`docker-machine env\` command."
+eval "$(docker-machine env dev)"
+
+docker_machine_ip=$(docker-machine ip dev)
+
+export DOCKER_MACHINE_IP=$docker_machine_ip
+
+echo "# You are all set.  Dev away."
+
